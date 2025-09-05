af6c67c69d8fd6f0e12c5eb447fbde97a4486e7d
Natthyx <natannanz16@gmail.com>
2025-09-05 18:41:26 +0300

    update: Update session path

--- app/telegram_client.py
+++ app/telegram_client.py
@@ -7,10 +7,8 @@ load_dotenv()
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 phone_no = os.getenv("PHONE_NUMBER")
-session_dir = "session"
-os.makedirs(session_dir, exist_ok=True)
-session_path = os.path.join(session_dir, "anon.session")
 
+session_path = os.getenv("SESSION_PATH", "session/anon.session")
 client = TelegramClient(session_path, api_id, api_hash)
 
 async def start_client():
